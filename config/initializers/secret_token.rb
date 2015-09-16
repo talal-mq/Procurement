0bfc33124843b451437891306c35b726a96f9de5
talal-mq <talal.shoeb@marafeq.com.qa>
2015-09-16 11:09:04 +0300

    Intial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Procurement::Application.config.secret_token = 'cc7c47a00165eb7f600394ff261172d270b64f93041acceae5c62a3739aeaf56b07eead1671d0584cbda83bbb695db52645ccd24b7f6aecab7929b2a5a1d142a'
